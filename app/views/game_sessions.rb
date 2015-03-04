79f60a9603f2bcb9ed5274a1e90f3e8c9e72a541
Matthew Coleman <kirocuto@gmail.com>
2015-03-04 14:08:29 -0500

    added a basic skeleton for viewing all recent game states

---
+++ app/views/game_sessions.rb
@@ -0,0 +1,25 @@
+ %h1 Game Sessions
+ %table.styled
+    %thead
+      %tr
+        %th Game
+        %th ID
+        %th Player
+        %th State
+
+    %tbody
+      %tr
+        %td Game1
+        %td 45678
+        %td Steve
+        %td 827PGTBF243GT
+      %tr
+        %td Game2
+        %td 5435
+        %td Steve
+        %td SRYNJ3Q5HW54YH
+      %tr
+        %td Game2
+        %td 4563
+        %td Jimbo
+        %td 546DRFT7GAH23
