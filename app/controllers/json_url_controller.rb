57a51e9ba84533def0476512f084ebb7ffe841f7
thurston <deanthurston2791@gmail.com>
2015-03-31 17:06:51 -0400

    more fixes

--- app/controllers/json_url_controller.rb
+++ app/controllers/json_url_controller.rb
@@ -5,44 +5,16 @@ class JsonUrlController < ApplicationController
 
   def new
   	 
-    @result = JSON.parse(open("http://localhost:10009/GL_VPAL_Interactions").read)
-    @names = []
-	i = 0
-    
-    m2 = [[]]
-    (0..200).each do |x|
-    m2[x] = []
-    	(0..15).each do |y|
-    	m2[x][y] = 0
-    	end
-    end
+    @rows = DataMatrix.getData();
+    m2 = @rows
 
-    @result[0].each do |dat|
- 		
-    	@names[i] = dat.first
-    	i = i + 1
-    end
-    p = 0
-    c = 0
-    l = 0
-    m2[c][0] = 0
-
-    @result.each_with_index do |dat, index|
-    	m2[index] = []
-    	
-    	dat.each_with_index do |num,index2|
-    	m2[index][index2] = num[1]
-    	end
-    end
-    @rows = m2
-
-    mat = Matrix[*m2]
+    @mat = Matrix[*m2]
     i = 0
      @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => "Comparing attribute 6")
 
       f.xAxis(:categories => [ 1 , 2, 3, 4, 5])
-      f.series(:name => "attribute 6", :yAxis => 0, :data => mat.column(1))
+      f.series(:name => "attribute 6", :yAxis => 0, :data => @mat.column(1))
       
       f.yAxis [
         {:title => {:text => "GDP in Billions", :margin => 70} }
